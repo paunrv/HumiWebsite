fbd6bbeb62e0c1284d645dd7424e9ccccf75c0ef
paunrv <pau.norv@gmail.com>
2015-04-24 21:59:57 -0700

    Unnecessary routes removed

--- controllers/index.js
+++ controllers/index.js
@@ -7,14 +7,5 @@ var express = require('express'),
 router.use('/', require('./grado'))
 router.use('/', require('./alumno'))
 
-router.get('/', function(req,res){
-	res.status(200);
-	res.render('index');
-});
-
-router.get('/alumnos', function(req,res){
-	res.status(200);
-	res.render('alumnos');
-});
 
 module.exports = router;
